3ee165bfc507112c5067918dca7b958d0d832e4a
sieste <stefan_siegert@gmx.de>
2016-08-09 00:14:42 +0100

    factor analysis mle and bic

--- R/functions.R
+++ R/functions.R
@@ -1,9 +1,9 @@
 #' Calculate factor analysis log-likelihood function
 #'
 
-fa_llik = function(x, Mu=rep(0,p), Lambda=rep(0,p), Psi=diag(p)) {
+fa_llik = function(x, Mu=rep(0,p), Lambda=matrix(0, p, 1), Psi=rep(1,p)) {
   p = ncol(x)
-  Sigma = tcrossprod(Lambda) + diag(c(Psi))
+  Sigma = tcrossprod(Lambda) + diag(c(Psi^2))
   dec = chol(Sigma)
   tmp = backsolve(dec, t(x) - Mu, transpose=TRUE)
   rss = colSums(tmp^2)
@@ -11,4 +11,31 @@ fa_llik = function(x, Mu=rep(0,p), Lambda=rep(0,p), Psi=diag(p)) {
   return(ans)
 }
 
+fa_llik_opt = function(par, x, n, p, nf) {
+  Mu = par[1:p]
+  Lambda = matrix(par[(p+1):((nf+1)*p)], ncol=nf)
+  Psi = par[((nf+1)*p+1):length(par)]
+  return(-sum(fa_llik(x=x, Mu=Mu, Lambda=Lambda, Psi=Psi)))
+}
+
+fa_mle = function(x, nf=1, par=c(rep(1, (nf+1)*p), rep(1, p)), control=list()) {
+  n = nrow(x)
+  p = ncol(x)
+  opt = optim(par=par, fn=fa_llik_opt, method='BFGS', x=x, n=n, p=p, nf=nf, control=control)
+  ans = list()
+  ans$Mu = opt$par[1:p]
+  ans$Lambda = matrix(opt$par[(p+1):((nf+1)*p)], ncol=nf)
+  ans$Psi2 = opt$par[((nf+1)*p+1):length(opt$par)]^2.
+  ans$convergence = opt$convergence
+  ans$nll = opt$value
+  ans$n = nrow(x)
+  return(ans)
+}
+
+fa_bic = function(opt) {
+  nf = ncol(opt$Lambda)
+  p = nrow(opt$Lambda)
+  k = (nf+2) * p - nf * (nf - 1) / 2
+  return(2. * opt$nll + log(opt$n) * k)
+}
 
--- R/test.R
+++ R/test.R
@@ -1,17 +1,20 @@
 source('functions.R')
-set.seed(123)
+library(mvtnorm)
+set.seed(12345)
 
 # artificial data
-x = matrix(rnorm(100), 20, 5)
-n = nrow(x)
-p = ncol(x)
-Lambda = rep(.5, p)
+n = 100
+p = 10
+Mu = rep(2, p)
+Lambda = matrix(rnorm(2*p)^2, p, 2)
 Psi = rep(1, p)
-Mu = rep(0, p)
+Sigma = tcrossprod(Lambda) + diag(c(Psi^2))
+
+x = rmvnorm(n=n, mean=Mu, sigma=Sigma)
 
 # test factor analysis log likelihood function
 ll1 = fa_llik(x, Mu, Lambda, Psi)
-ll2 = mvtnorm::dmvnorm(x, Mu, tcrossprod(Lambda)+diag(c(Psi)), log=TRUE)
+ll2 = mvtnorm::dmvnorm(x, Mu, Sigma, log=TRUE)
 stopifnot(all(ll1 == ll2))
 
 # microbenchmark::microbenchmark(
@@ -20,5 +23,10 @@ stopifnot(all(ll1 == ll2))
 # )
 # 75 vs 170 microseconds
 
+print(fa_mle(x, control=list(maxit=1e6)))
 
+for (nf in 1:4) {
+  opt = fa_mle(x, nf)
+  print(fa_bic(opt))
+}
 
